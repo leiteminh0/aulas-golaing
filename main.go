66d4a5c4f06e6c1c38726cec439ad4ad0cbf54c4
Duda <maria.squeruque@aluno.senai.br>
2025-03-28 08:48:40 -0300

    num1, num2

--- main.go
+++ main.go
@@ -3,9 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	a, b := 10, 3
-	fmt.Println(" A soma é:", a+b)
-	fmt.Println(" a subtração é:", a-b)
-	fmt.Println(" a multiplicação é:", a/b)
-	fmt.Println("o resto da divisão:", a%b)
+	var num1, num2  float64
+	
+	fmt.Print("Digite o primeiro numero:")
+	fmt.Scan(&num1)
+
+	fmt.Print("Digite o segundo numero:")
+	fmt.Scan(&num2)
+
+	sum:= num1 + num2
+	diff:= num1 - num2
+	prod:= num1 * num2
+
+	fmt.Println("\nResultados:")
+	fmt.Printf("Soma: %.2f\n", sum)
+	fmt.Printf("Subtração: %.2f\n", diff)
+	fmt.Printf("Multiplicação: %.2f\n", prod)
+
+	f num2 != 0 {
+		quot := num1 / num2
+		fmt.Printf("Divisão: %.2f\n", quot)
+	} else {
+		fmt.Println("Divisão por zero não é permitida.")
+	}
+	if num2 != 0 {
+		mod := int(num1) % int(num2)
+		fmt.Printf("Módulo: %d\n", mod)
+	} else {
+		fmt.Println("Módulo por zero não é permitido.")
+	}
 }
