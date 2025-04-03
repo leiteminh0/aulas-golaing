11d763016acabaf73678e9e851ddc4f407f8d773
Duda <maria.squeruque@aluno.senai.br>
2025-04-03 09:24:18 -0300

    vetore superherois

--- main.go
+++ main.go
@@ -9,4 +9,19 @@ func main() {
 	fmt.Println(nomes)
 	nomes[3] = "Clark Kent"
 	fmt.Println(nomes)
+	// Slice
+	var score = []int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2
+	fmt.println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+	var superherois = []string{"deadpool", "homem-aranha", "motoqueiro fantasma"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "Ben 10", "mulher maravilha")
+	fmt.Println(superherois, len(superherois), cap(superherois))
 }
