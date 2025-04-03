c760cdfdea386a9176a09187f7f4cfdabab8833b
Duda <maria.squeruque@aluno.senai.br>
2025-04-03 08:27:20 -0300

    vetores

--- main.go
+++ main.go
@@ -3,33 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2  float64
-	
-	fmt.Print("Digite o primeiro numero:")
-	fmt.Scan(&num1)
-
-	fmt.Print("Digite o segundo numero:")
-	fmt.Scan(&num2)
-
-	sum:= num1 + num2
-	diff:= num1 - num2
-	prod:= num1 * num2
-
-	fmt.Println("\nResultados:")
-	fmt.Printf("Soma: %.2f\n", sum)
-	fmt.Printf("Subtração: %.2f\n", diff)
-	fmt.Printf("Multiplicação: %.2f\n", prod)
-
-	f num2 != 0 {
-		quot := num1 / num2
-		fmt.Printf("Divisão: %.2f\n", quot)
-	} else {
-		fmt.Println("Divisão por zero não é permitida.")
-	}
-	if num2 != 0 {
-		mod := int(num1) % int(num2)
-		fmt.Printf("Módulo: %d\n", mod)
-	} else {
-		fmt.Println("Módulo por zero não é permitido.")
-	}
+	var ages = [4]int{17, 16, 20, 40}
+	nomes := [4]string{"mario", "luigi", "deadpool", "superman"}
+	fmt.Println(ages)
+	fmt.Println(nomes)
+	nomes[3] = "Clark Kent"
+	fmt.Println(nomes)
 }
